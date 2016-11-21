039aa2ccfbd3d4c4e124af1b99127479fa863a19
linsheng <986992484@qq.com>
2016-11-22 02:31:23 +0800

    JS智能选择

--- App/Runtime/Cache/Admin/ae4e3fe6a66539908fe90517dc97b7e4.php
+++ App/Runtime/Cache/Admin/ae4e3fe6a66539908fe90517dc97b7e4.php
@@ -12,6 +12,43 @@
 	<script type="text/javascript" src="/E-Cshop/Public/bootstrap/js/jquery.min.js"></script>
 	<!--其它样式-->
 	
+    <script type="text/javascript">
+       $(function(){
+            //JS智能选择
+            // 为所有的选择框绑定点击事件
+            $(":checkbox").click(function(){       
+                var _this=this;
+                // 先取出当前权限的level值是多少
+                var cur_level = $(this).attr("level");
+                var tmplevel = cur_level; // 给一个临时的变量后面要进行减操作
+                // 先取出这个复选框所有前面的复选框
+                var allprev = $(this).prevAll(":checkbox");
+                // 循环每一个前面的复选框判断是不是上级的
+                $(allprev).each(function(k,v){
+                    // 判断是不是上级的权限
+                    if($(v).attr("level") < tmplevel){
+                        $(v).get(0).checked=$(_this).get(0).checked;
+                        tmplevel--; // 再向上提一级               
+                    }
+                    else return false; 
+                });
+                // 所有子权限也选中
+                // 先取出这个复选框所有前面的复选框
+                var allnext = $(this).nextAll(":checkbox");
+                // 循环每一个前面的复选框判断是不是上级的
+                $(allnext).each(function(k,v){
+                // 判断是不是上级的权限
+                    if($(v).attr("level") > cur_level){
+                         $(v).get(0).checked=$(_this).get(0).checked;
+                         tmplevel++;
+                    } 
+                    // 遇到一个平级的权限就停止循环后面的不用再判断了
+                    else return false;                 
+                });
+            });   
+       });  
+    </script>
+
 </head>
 <body>
 <h1 style="font-size:14px">
@@ -31,11 +68,11 @@
     <div class="main-div">
         <form method="POST" style="margin:5px" action="/E-Cshop/Role/add.html">
             <p>角色名称：<input  type="text" name="role_name"/></p>
-            <p>选择该角色拥有的所有权限:
+            <p>为该角色分配权限:
             	<div class="alert alert-info">
 	            <?php if(is_array($data)): $i = 0; $__LIST__ = $data;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$obj): $mod = ($i % 2 );++$i; echo str_repeat('-',$obj['level']*8);?>
 					<input type="checkbox" name="auth_id[]" style="margin:5px" 
-					value="<?php echo ($obj["id"]); ?>"><?php echo ($obj["auth_name"]); ?><br/><?php endforeach; endif; else: echo "" ;endif; ?>
+					value="<?php echo ($obj["id"]); ?>" level="<?php echo ($obj["level"]); ?>"><?php echo ($obj["auth_name"]); ?><br/><?php endforeach; endif; else: echo "" ;endif; ?>
 				</div>
             </p>
             <p><input type="submit" class="btn btn-primary" value="确定"/></p>
