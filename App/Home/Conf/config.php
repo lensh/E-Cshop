172b8ded38080bfad6fcbb7dd773e5748bed9653
linsheng <986992484@qq.com>
2016-12-02 18:46:40 +0800

    生成静态页

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -2,12 +2,14 @@
 return array(
 	//页面Trace
 	'SHOW_PAGE_TRACE'=>true,	
-	//启用路由功能
-	'URL_ROUTER_ON'=>true,
-	//配置路由规则
-	'URL_ROUTE_RULES'=>array(
-		//每条键值对，对应一个路由规则
-		'i/:domain'=>'Space/index',
-		//对应的URL就是Space/index/i/xiaoxin
-	)
+	//静态缓存
+	'HTML_CACHE_ON'=> true, // 开启静态缓存
+	'HTML_CACHE_TIME'=>60, // 全局静态缓存有效期（秒）
+	'HTML_FILE_SUFFIX'=>'.shtml', //设置静态缓存文件后缀
+    // 定义静态缓存规则
+	'HTML_CACHE_RULES'=>array(  // 定义静态缓存规则，为前台的每个页面单独配置    
+		 //  Index:index是Index控制器的index方法,后面的array的第一个参数
+		 //是指生成的静态文件名,第二个参数是指有效时间
+		 'Index:index'=>array('index',3600)
+	 )
 );
